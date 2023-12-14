50a246140682941ed22cff9be9ce04fdea0e6c2a
emreyazicicode <133089486+emreyazicicode@users.noreply.github.com>
2023-12-14 11:22:30 +0300

    Add files via upload

--- week1/w1l.py
+++ week1/w1l.py
@@ -1,3 +1,17 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 marks = {
     'Anar Gasimov': 78,
     'Fariz Azimov': 71,
@@ -14,3 +28,38 @@ marks = {
     'Ulvi Hasanov': 82,
     'Shargiyya Mahmudzada': 87
 }
+
+indiye_kadar_olan_en_buyuk_skor = 0
+
+"""
+her bir item a bakacagiz
+eger o itemin skoru, indiye_kadar_olan_en_buyuk_skor dan buyukse
+indiye_kadar_olan_en_buyuk_skor = o itemin skoru
+"""
+
+for talebe in marks: # in dictionary, for gives the "KEY"
+    key = talebe
+    val = marks[talebe]
+    if val > indiye_kadar_olan_en_buyuk_skor:
+        indiye_kadar_olan_en_buyuk_skor = val
+
+
+print(indiye_kadar_olan_en_buyuk_skor)
+
+# =====================
+
+l = [45,54,67,76,87,89,0]
+indiye_kadar_olan_en_buyuk_value = 0
+
+for pinokyo in l:
+    if pinokyo > indiye_kadar_olan_en_buyuk_value:
+        indiye_kadar_olan_en_buyuk_value = pinokyo
+
+print("indiye_kadar_olan_en_buyuk_value", indiye_kadar_olan_en_buyuk_value)
+
+#! print(max(l))
+#! print(sorted(l)[-1])
+
+
+
+
---
+++ week1/w1n.py
@@ -0,0 +1,25 @@
+
+marks = {
+    'Anar Gasimov': 78,
+    'Fariz Azimov': 71,
+    'Fatima Umudova': 87,
+    'Fazil Zeynalov': 93,
+    'Kamilla Mirzeyeva': 91,
+    'Khagani Gasimov': 79,
+    'Nigar Jahangirli': 76,
+    'Orkhan Mustafayev': 92,
+    'Sabina Muradzada': 94,
+    'Samir Suleymanov': 88,
+    'Sevinj Amanova': 81,
+    'Ujar Ismayilzada': 82,
+    'Ulvi Hasanov': 82,
+    'Shargiyya Mahmudzada': 87
+}
+
+names = list(marks.keys())
+print(names)
+
+
+
+
+
